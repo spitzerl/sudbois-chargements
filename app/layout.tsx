35672565d14c6a7b5ede9ec67c042df6ecde9f7f
Lucas Spitzer <spitzer.lucas@proton.me>
2025-10-17 17:51:01 +0200

    Ajout de l'importation de globals.css dans le fichier layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 import { Navbar } from "@/components/layout/Navbar";
 
 const geistSans = Geist({
